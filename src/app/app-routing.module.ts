3752140ea964ca431a62962524a04204e5212703
NabilAbde <80818068+NabilAbde@users.noreply.github.com>
2021-08-02 08:45:14 +0100

    suite routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MontantTotalVentesComponent } from './montant-total-ventes/montant-total-ventes.component';
+import { NbrCommandesComponent } from './nbr-commandes/nbr-commandes.component';
+import { NbrPaniersComponent } from './nbr-paniers/nbr-paniers.component';
+import { ValeurPanierMoyenComponent } from './valeur-panier-moyen/valeur-panier-moyen.component';
 
 const routes: Routes = [
   { path: "" , component: HomePageComponent},
-  {path: "montanttotaldesventes" , component : MontantTotalVentesComponent}
-
+  {path: "montanttotaldesventes" , component : MontantTotalVentesComponent},
+  {path: "nbrcommandes" , component : NbrCommandesComponent},
+  {path: "nbrpanniers" , component : NbrPaniersComponent },
+  {path: "panniermoyen" , component : ValeurPanierMoyenComponent},
 ];
 
 @NgModule({
