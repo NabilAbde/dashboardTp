7718b5da2c062151a24617f0e4d45ffe6c908423
NabilAbde <80818068+NabilAbde@users.noreply.github.com>
2021-08-02 08:58:18 +0100

    Lien route et active linf ok tout component
    routerLinkActive et router link creation tag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MontantTotalVentesComponent } from './montant-total-ventes/montant-total-ventes.component';
 import { NbrCommandesComponent } from './nbr-commandes/nbr-commandes.component';
+import { NbrNewClientsComponent } from './nbr-new-clients/nbr-new-clients.component';
 import { NbrPaniersComponent } from './nbr-paniers/nbr-paniers.component';
+import { TauxPanierLostComponent } from './taux-panier-lost/taux-panier-lost.component';
+import { TauxRecComClientsComponent } from './taux-rec-com-clients/taux-rec-com-clients.component';
+import { TauxVisitPanierNewComponent } from './taux-visit-panier-new/taux-visit-panier-new.component';
+import { TotalProductssellsComponent } from './total-productssells/total-productssells.component';
 import { ValeurPanierMoyenComponent } from './valeur-panier-moyen/valeur-panier-moyen.component';
 
 const routes: Routes = [
@@ -12,6 +17,12 @@ const routes: Routes = [
   {path: "nbrcommandes" , component : NbrCommandesComponent},
   {path: "nbrpanniers" , component : NbrPaniersComponent },
   {path: "panniermoyen" , component : ValeurPanierMoyenComponent},
+  {path: "nbrnewclients" , component : NbrNewClientsComponent},
+  {path: "tauxabandon" , component : TauxPanierLostComponent},
+  {path: "tauxreccommandes" , component : TauxRecComClientsComponent},
+  {path: "tauxvisitepannier" , component : TauxVisitPanierNewComponent},
+  {path: "totaltproduits" , component : TotalProductssellsComponent}
+  
 ];
 
 @NgModule({
